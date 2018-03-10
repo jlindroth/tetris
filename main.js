c3c2a35a04c893b840c4504980ae9e1017a56491
Joakim Lindroth <jlindroth@gmail.com>
2018-03-10 14:48:21 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//Tetris with multiplayer
+//For browsers using Phaser
+
+//1920x1080 AUTO |CANVAS | AUTO | WEBGL
+var game = new Phaser.Game(1920,900, Phaser.CANVAS, '', {preload: preload, create: create, update: update });
+
+function preload(){}
+function create(){}
+function update(){}
