ef8ebf5d9c70367cc9861d5170f034715c2d757c
Joakim Lindroth <jlindroth@gmail.com>
2018-03-11 18:24:58 +0100

    not working

--- main.js
+++ main.js
@@ -3,12 +3,19 @@
 
 //1920x1080 AUTO |CANVAS | AUTO | WEBGL
 var game = new Phaser.Game(1920,900, Phaser.CANVAS, '', {preload: preload, create: create, update: update });
-var bmp;
+var bmp, sprit;
 function preload(){}
 function create(){
     //Skapa en bitmap
     bmp = game.make.bitmapData(1,1);
-    bmp.pixels[]
+    bmp.data[0] = 0xFF0000;
+    bmp.update();
+    sprit = game.add.sprite(200,200,bmp);
+    sprit.width = 10;
+    sprit.height = 10;
     //Skapa en sprite från bitmap
 }
-function update(){}
+function update(){
+
+
+}
